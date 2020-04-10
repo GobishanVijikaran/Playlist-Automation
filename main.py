e64f29a9350cb2c52e1ff618a4f38146db766f8b
Gobishan Vijikaran <gvijikar@edu.uwaterloo.ca>
2020-04-09 23:11:41 -0400

    Added search for track capabilities with Spotify API

--- main.py
+++ main.py
@@ -22,7 +22,20 @@ class PlaylistAutomation:
         pass
 
     def get_song(self):
-        pass
+
+        song_name = "toosie slide"
+        song_type = "track"
+        song_market = "US"
+
+        get_song_query = f"https://api.spotify.com/v1/search?q={song_name}&type={song_type}&market={song_market}&limit=1"
+        get_song = requests.get(url=get_song_query, headers=http_header)
+        get_song_mod = get_song.json()
+        song_id = get_song_mod["tracks"]["items"][0]["uri"]
+
+        # Adding to Playlist
+        track_query = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks?uris={song_id.replace(':', '%3A')}"
+        add_tracks = requests.post(url=track_query, headers=http_header)
+        print(add_tracks)
 
     def spotify_playlist(self):
         query = f"https://api.spotify.com/v1/users/{self.spotify_user_id}/playlists"
